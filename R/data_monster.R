e4d5111e1bdba49d0a7f2c1e209c1e28fa9cd606
Stéphane Laurent <laurent_step@yahoo.fr>
2023-02-01 00:24:04 +0100

    monster data

---
+++ R/data_monster.R
@@ -0,0 +1,8 @@
+#' @title Monster head
+#'
+#' @description A list representing the head of a monster as a polygon with
+#'   holes.
+#'
+#' @format A list with two fields: a matrix \code{"outer"} and a list 
+#'   \code{"holes"}, containing three matrices.
+"monster"
