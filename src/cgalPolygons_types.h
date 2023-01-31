391fb2711f3f9a80804577eca291755865b3cb49
Stéphane Laurent <laurent_step@yahoo.fr>
2023-01-31 19:49:00 +0100

    changed polygon type

--- src/cgalPolygons_types.h
+++ src/cgalPolygons_types.h
@@ -1,6 +1,11 @@
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <CGAL/Polygon_2.h>
+//#include <CGAL/Polygon_2.h>
+#include <CGAL/Partition_traits_2.h>
+#include <CGAL/partition_2.h>
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel EK;
-typedef EK::Point_2                                       Point;
-typedef CGAL::Polygon_2<EK>                               Polygon;
+//typedef EK::Point_2                                       Point;
+//typedef CGAL::Polygon_2<EK>                               Polygon;
+typedef CGAL::Partition_traits_2<EK>                        Traits;
+typedef Traits::Point_2                                     Point;
+typedef Traits::Polygon_2                                   Polygon;
