d8d33769e6067da8545acf4832190c23c126ef74
Stéphane Laurent <laurent_step@yahoo.fr>
2023-01-31 20:41:27 +0100

    code cleaning

--- src/cgalPolygons.h
+++ src/cgalPolygons.h
@@ -3,17 +3,15 @@
 #endif
 
 #include <Rcpp.h>
-
 #define CGAL_EIGEN3_ENABLED 1
-
 #include "cgalPolygons_types.h"
-
 #include <CGAL/Bbox_2.h>
 #include <CGAL/enum.h>
 
-
+// -------------------------------------------------------------------------- //
 typedef Polygon::Vertex_iterator VertexIterator;
 
+// -------------------------------------------------------------------------- //
 void Message(const std::string&);
 Polygon makePolygon(const Rcpp::NumericMatrix&);
 Rcpp::NumericMatrix getVertices(const Polygon&);
--- src/cgalPolygons_types.h
+++ src/cgalPolygons_types.h
@@ -1,11 +1,13 @@
+#ifndef _HEADER_TYPES_
+#define _HEADER_TYPES_
+
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-//#include <CGAL/Polygon_2.h>
 #include <CGAL/Partition_traits_2.h>
 #include <CGAL/partition_2.h>
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel EK;
-//typedef EK::Point_2                                       Point;
-//typedef CGAL::Polygon_2<EK>                               Polygon;
 typedef CGAL::Partition_traits_2<EK>                      Traits;
 typedef Traits::Point_2                                   Point;
 typedef Traits::Polygon_2                                 Polygon;
+
+#endif
