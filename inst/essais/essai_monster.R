e5d43200c0bc72d3620b070b0fcb97346d322704
Stéphane Laurent <laurent_step@yahoo.fr>
2023-02-01 01:07:10 +0100

    monster png

--- inst/essais/essai_monster.R
+++ inst/essais/essai_monster.R
@@ -11,10 +11,14 @@ library(cgalPolygons)
 pwh <- cgalPolygonWithHoles$new(monster[["outer"]], monster[["holes"]])
 cxparts <- pwh$convexParts(method = "triangle")
 
+svglite::svglite("monster.svg", width = 8, height = 8)
 par(mar = c(0, 0, 0, 0))
-pwh$plot(list(col = "pink"), col = "yellow")
+pwh$plot(list(col = "indianred"), col = "yellow3")
 invisible(
   lapply(cxparts, function(cxpart) {
     polygon(cxpart, lwd = 2)
   })
 )
+dev.off()
+
+rsvg::rsvg_png("monster.svg", "monster.png", width = 512, height = 512)
