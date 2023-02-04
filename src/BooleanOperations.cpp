a6a1ff0a0e7f226963c0cbeb1346e7a30657dd3b
Stéphane Laurent <laurent_step@yahoo.fr>
2023-02-04 15:09:30 +0100

    message number of holes in Intersection()

--- src/BooleanOperations.cpp
+++ src/BooleanOperations.cpp
@@ -15,17 +15,36 @@ Rcpp::List Intersection(
   CGAL::intersection(pgn1, pgn2, std::back_inserter(pgnsList));
   int npgns = std::distance(pgnsList.begin(), pgnsList.end());
   if(npgns == 0) {
-    Message("The intersection is empty.\n");
+    Message("The intersection is empty.");
     return Rcpp::List::create();
   } else if(npgns == 1) {
-    Message("The intersection consists in one polygon.\n");
+    Message("The intersection consists in one polygon.");
+    int nholes = pgnsList[0].number_of_holes();
+    if(nholes == 0) {
+      Message("It has no hole.");
+    } else if(nholes == 1) {
+      Message("It has one hole.");
+    } else {
+      Message("It has " + std::to_string(nholes) + " holes.");
+    }
   } else {
     Message("The intersection consists in " + 
-              std::to_string(npgns) + " polygons.\n");
+              std::to_string(npgns) + " polygons.");
   }
   Rcpp::List out(npgns);
   for(int i = 0; i < npgns; i++) {
     out(i) = returnPolygonWithHoles(pgnsList[i]);
+    if(npgns > 1) {
+      int nholes = pgnsList[i].number_of_holes();
+      if(nholes == 0) {
+        Message("Polygon " + std::to_string(i+1) + " has no hole.");
+      } else if(nholes == 1) {
+        Message("Polygon " + std::to_string(i+1) + " has one hole.");
+      } else {
+        Message("Polygon " + std::to_string(i+1) + " has " + 
+                  std::to_string(nholes) + " holes.");
+      }
+    }
   }
   return out;
 }
